924f0da3232d49dc1700001f1b2eebb7b375a884
PaavoNykanen <paavonykanen@gmail.com>
2024-04-16 14:07:25 +0300

    part 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,18 +5,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.info} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part info={props.part1} exercises={props.exercises1} />
+      <Part info={props.part2} exercises={props.exercises2} />
+      <Part info={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
