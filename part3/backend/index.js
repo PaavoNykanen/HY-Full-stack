c060b92da0a187c488ab129e2d225168d7be9350
PaavoNykanen <paavonykanen@gmail.com>
2024-04-28 17:38:09 +0300

    part 3.7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
 
 let notes = [
   {
@@ -19,6 +18,17 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+  
+app.use(express.json())
+app.use(requestLogger)
 
 const generateId = () => {
     const maxId = notes.length > 0
