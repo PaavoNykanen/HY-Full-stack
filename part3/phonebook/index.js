3252d11eaf96f836b26168c8a1f196045f38b029
PaavoNykanen <paavonykanen@gmail.com>
2024-05-02 17:27:41 +0300

    part 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 //app.use(morgan('tiny'))
 app.use(morgan(function (tokens, req, res) {
@@ -19,7 +20,6 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 app.use(cors())
-app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -69,13 +69,14 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     response.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
   .then(result => {
     response.status(204).end()
@@ -99,6 +100,23 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
