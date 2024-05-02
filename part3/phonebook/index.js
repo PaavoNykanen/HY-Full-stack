bc21fd68c258e0f22077310cd19e6c2314caee75
PaavoNykanen <paavonykanen@gmail.com>
2024-05-02 17:38:08 +0300

    part 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,34 +21,6 @@ app.use(morgan(function (tokens, req, res) {
   }))
 app.use(cors())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-    const id = Math.floor(Math.random() * 1000000);
-    return id
-}
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
