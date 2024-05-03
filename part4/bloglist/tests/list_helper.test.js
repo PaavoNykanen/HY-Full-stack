0ad84dd5c5d9552d1605858219b96be62b961b41
PaavoNykanen <paavonykanen@gmail.com>
2024-05-03 12:50:32 +0300

    part 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -71,3 +71,66 @@ describe('total likes', () => {
     assert.strictEqual(result, 10)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const biggerListWithBlogs = [
+    {
+      _id: '1',
+      title: 'Test blog',
+      author: 'Author name',
+      url: 'www.con',
+      likes: 1,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Blog 2',
+      author: 'MR Author',
+      url: 'https://homepages.com',
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Book',
+      author: 'Edger',
+      url: 'github.com',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '4',
+      title: 'HY FullStack',
+      author: 'Paavo Nykänen',
+      url: 'jyu.fi',
+      likes: 4,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog, is that one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+
+  test('of a bigger list is the highes like count blog', () => {
+    const result = listHelper.favoriteBlog(biggerListWithBlogs)
+    assert.deepStrictEqual(result, biggerListWithBlogs[3])
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,8 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  return blogs.reduce((favorite, currBlog) =>
+    favorite.likes > currBlog.likes ? favorite : currBlog, blogs[0])
+}
+
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
