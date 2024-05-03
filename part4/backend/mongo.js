d9ed52a9f540f71f1c59ad80485989906abfeed6
PaavoNykanen <paavonykanen@gmail.com>
2024-05-03 15:54:53 +0300

    part 4.8

--- part4/backend/mongo.js
+++ part4/backend/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://paavonykanen:${password}@cluster1.intudws.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster1`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,22 +19,23 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  /*
+
   const note = new Note({
     content: 'HTML is x',
     important: true,
   })
 
+  // eslint-disable-next-line no-unused-vars
   note.save().then(result => {
     console.log('note saved!')
     mongoose.connection.close()
   })
-  */
-  Note.find({}).then(result => {
+
+  /*Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
     })
     mongoose.connection.close()
-  })
+  })*/
 })
 
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Test Blog 1',
+    author: 'Test Author 1',
+    url: 'url.com',
+    likes: 2
+  },
+  {
+    title: 'Test Blog 2',
+    author: 'Test Author 2',
+    url: 'blog.fi',
+    likes: 0
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
