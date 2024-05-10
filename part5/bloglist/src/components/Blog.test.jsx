a564d98dec3e0354e331bd95598e2590705fb946
PaavoNykanen <paavonykanen@gmail.com>
2024-05-10 13:29:09 +0300

    part 5.14

--- part5/bloglist/src/components/Blog.test.jsx
+++ part5/bloglist/src/components/Blog.test.jsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import { expect } from 'chai'
+import userEvent from '@testing-library/user-event'
 
-test('renders minimal blog content', async () => {
+describe('Blog component', () => {
 
+  let container
   const blog = {
     title: 'Test blog',
     author: 'Test author',
@@ -14,18 +16,40 @@ test('renders minimal blog content', async () => {
   const likeHandler = vi.fn()
   const deleteHandler = vi.fn()
 
+  beforeEach(() => {
+    container = render(
+      <Blog blog={blog} user={null} updateBlog={likeHandler} removeBlog={deleteHandler} />
+    ).container
+  })
 
-  const container = render(
-    <Blog blog={blog} user={null} updateBlog={likeHandler} removeBlog={deleteHandler} />
-  ).container
+  test('renders minimal blog content initially', async () => {
 
-  const component = container.querySelector('.blogContainer')
-  expect(component).toBeDefined()
+    const component = container.querySelector('.blogContainer')
+    expect(component).toBeDefined()
 
-  expect(component).toHaveTextContent('Test blog')
-  expect(component).toHaveTextContent('Test author')
+    expect(component).toHaveTextContent('Test blog')
+    expect(component).toHaveTextContent('Test author')
 
-  expect(component).not.toHaveTextContent('http://test.com')
-  expect(component).not.toHaveTextContent('0')
+    expect(component).not.toHaveTextContent('http://test.com')
+    expect(component).not.toHaveTextContent('0')
+
+  })
+
+  test('renders all blog content when view button is pressed', async () => {
+
+    const component = container.querySelector('.blogContainer')
+    expect(component).toBeDefined()
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(component).toHaveTextContent('Test blog')
+    expect(component).toHaveTextContent('Test author')
+
+    expect(component).toHaveTextContent('http://test.com')
+    expect(component).toHaveTextContent('0')
+
+  })
 
 })
